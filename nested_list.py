18d39160da9aa87ed9a5211871d3ca35acb1f90b
Ashwani Raj <88542636+Ashwaniraj679@users.noreply.github.com>
2022-10-07 23:32:03 +0530

    updated

--- nested_list.py
+++ nested_list.py
@@ -1,10 +1,16 @@
 if __name__ == '__main__':
     students = []
+    marks = []
     for i  in range(int(input())):
         name = input()
         score = float(input())
         students.append([name,score])
+        if score not in marks:
+            marks.append(score)
 students.sort(key=lambda x: (x[1],x[0]))
+marks.sort()
+marks.pop(0)
+sec_low = marks[0]
 for x in students:
-    if students[1][1] == x[1]:
+    if x[1] == sec_low:
         print(x[0])
